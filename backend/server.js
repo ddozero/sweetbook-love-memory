f0849a6c498ec9270b4537cdda450ca5cc387667
doyeong <kdoyeong1@gmail.com>
2026-04-10 15:15:09 +0900

    ✨ Feat: add express server and test API
    Express서버 생성 및 testAPI 추가

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// routes 나중에 만들면 여기 추가
+// const booksRouter = require('./routes/books');
+// const ordersRouter = require('./routes/orders');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// routes 나중에 만들면 여기 추가
+// app.use('/api/books', booksRouter);
+// app.use('/api/orders', ordersRouter);
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: '백엔드 서버 연결 성공!',
+  });
+});
+
+//서버 실행
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
